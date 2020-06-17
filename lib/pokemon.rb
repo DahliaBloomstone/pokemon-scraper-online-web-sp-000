ecba010b580c0cf747336b3eb31678c28bb3537f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-17 01:51:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,10 @@ class Pokemon
 attr_accessor :id, :name, :type, :db
 
 def initialize(id = nil, name, type, db)
+  @id = id
   @name = name
   @type = type
-  @db = db
-  @id = id
+  @db = db 
 end
 
 end
